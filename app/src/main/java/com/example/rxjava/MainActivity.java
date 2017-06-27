faf6f427f3db5e05a94eefd43e695b4586281059
YaoPaine <yaohe1991@gmail.com>
2017-06-27 10:21:04 +0800

    rxjava初体验

---
+++ app/src/main/java/com/example/rxjava/MainActivity.java
@@ -0,0 +1,103 @@
+package com.example.rxjava;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.disposables.Disposable;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        /**
+         * 上游   Observable
+         */
+
+        //创建一个上游
+        Observable<Integer> observable = Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(@NonNull ObservableEmitter<Integer> e) throws Exception {
+                e.onNext(1);
+                e.onNext(2);
+                e.onNext(3);
+                e.onNext(4);
+                e.onComplete();
+            }
+        });
+
+        /**
+         * 下游   Observer
+         */
+
+        Observer<Integer> observer = new Observer<Integer>() {
+
+            @Override
+            public void onSubscribe(@NonNull Disposable d) {
+                Log.e(TAG, "onSubscribe");
+            }
+
+            @Override
+            public void onNext(@NonNull Integer integer) {
+                Log.e(TAG, "onNext: " + integer);
+            }
+
+            @Override
+            public void onError(@NonNull Throwable e) {
+                Log.e(TAG, "onError");
+            }
+
+            @Override
+            public void onComplete() {
+                Log.e(TAG, "onComplete");
+            }
+        };
+
+        //建立连接
+        observable.subscribe(observer);
+
+        chain();
+    }
+
+    public void chain() {
+        Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(@NonNull ObservableEmitter<Integer> e) throws Exception {
+                e.onNext(4);
+                e.onNext(3);
+                e.onNext(2);
+                e.onNext(1);
+                e.onComplete();
+            }
+        }).subscribe(new Observer<Integer>() {
+            @Override
+            public void onSubscribe(@NonNull Disposable d) {
+                Log.e(TAG, "onSubscribe");
+            }
+
+            @Override
+            public void onNext(@NonNull Integer integer) {
+                Log.e(TAG, "onNext: " + integer);
+            }
+
+            @Override
+            public void onError(@NonNull Throwable e) {
+                Log.e(TAG, "onError");
+            }
+
+            @Override
+            public void onComplete() {
+                Log.e(TAG, "onComplete");
+            }
+        });
+    }
+}
